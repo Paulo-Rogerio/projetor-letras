504ff48243d78fad6c5b04c295cb8554c37f93b6
Paulo Rogério <paulo@assembleia.go.gov.br>
2017-01-02 14:34:28 -0200

    Ajustes nas rotas

--- config/routes.rb
+++ config/routes.rb
@@ -21,13 +21,7 @@ Rails.application.routes.draw do
   get '/reunioes(.:format)', to: 'reunioes#index', as: 'reunioes'
   get '/reunioes/:id(.:format)', to: 'reunioes#show', as: 'reuniao'
 
-  
-  # resources :temas
-  # resources :predios
-  # resources :preletores
-  # resources :funcoes
-  # resources :reunioes
-  
+    
   get 'creatives/index'
   root "creatives#index"
 
